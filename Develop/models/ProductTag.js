85f41323c0f90d5bc9595b499c3679290e0bfb4d
very john <armaanjelly@gmail.com>
2024-02-08 22:18:03 -0800

    add id and tags

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -1,19 +1,39 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
 
 class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    // Define columns for the product_tag table
+    id: {
+      type: DataTypes.INTEGER, // Set the data type to INTEGER
+      allowNull: false, // Disallow NULL values
+      primaryKey: true, // Set as the primary key
+      autoIncrement: true, // Automatically increment the value for each new record
+    },
+    tag_id: {
+      type: DataTypes.INTEGER, // Set the data type to INTEGER
+      references: {
+        model: "tag", // Reference the 'tag' table
+        key: "id", // Reference the 'id' column in the 'tag' table
+      },
+    },
+    product_id: {
+      type: DataTypes.INTEGER, // Set the data type to INTEGER
+      references: {
+        model: "product", // Reference the 'product' table
+        key: "id", // Reference the 'id' column in the 'product' table
+      },
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'product_tag',
+    modelName: "product_tag",
   }
 );
 
