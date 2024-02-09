a58daed9367540043c10d9eb90aebe545a3ef35d
very john <armaanjelly@gmail.com>
2024-02-08 22:20:46 -0800

    add more codes

--- Develop/models/Tag.js
+++ Develop/models/Tag.js
@@ -1,20 +1,40 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection.js');
+const sequelize = require("../config/connection");
 
-class Tag extends Model {}
+class ProductTag extends Model {}
 
-Tag.init(
+ProductTag.init(
   {
-    // define columns
+    // Define columns for the product_tag table
+    id: {
+      type: DataTypes.INTEGER, // Set the data type to INTEGER
+      allowNull: false, // Disallow NULL values
+      primaryKey: true, // Set as the primary key
+      autoIncrement: true, // Automatically increment the value for each new record
+    },
+    tag_id: {
+      type: DataTypes.INTEGER, // Set the data type to INTEGER
+      references: {
+        model: "tag", // Reference the 'tag' table
+        key: "id", // Reference the 'id' column in the 'tag' table
+      },
+    },
+    product_id: {
+      type: DataTypes.INTEGER, // Set the data type to INTEGER
+      references: {
+        model: "product", // Reference the 'product' table
+        key: "id", // Reference the 'id' column in the 'product' table
+      },
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'tag',
+    modelName: "product_tag",
   }
 );
 
-module.exports = Tag;
+module.exports = ProductTag;
